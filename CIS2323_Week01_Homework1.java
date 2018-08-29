6226cd8d74cf8b41188202ff9044988f1c634b7e
Bibek Gupta <bibek.gupta@okstate.edu>
2018-08-29 08:56:16 -0500

    Initial commit

---
+++ CIS2323_Week01_Homework1.java
@@ -0,0 +1,12 @@
+public class CIS2323_Week01_Homework1 {
+	public static void main(String[] args){
+		System.out.println("############################################################################");
+		System.out.println("Bibek Gupta");
+		System.out.println("Student, Oklahoma State University - Oklahoma City");
+		System.out.println("");
+		System.out.println("6413 N Warren Ave			p: (405)430-6995");
+		System.out.println("Oklahoma City, OK			e: bibek.gupta@okstate.edu");
+		System.out.println("					g: github.com/bbekjava");
+		System.out.println("############################################################################");
+	}
+}
